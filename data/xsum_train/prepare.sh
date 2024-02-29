25fbb6d6d2c3f026a0a51bce446266a9380a17fe
Medha Palavalli <palavall@br014.ib.bridges2.psc.edu>
2024-02-29 10:01:43 -0500

    some of xsum

--- data/xsum_train/prepare.sh
+++ data/xsum_train/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=prepare
-#SBATCH --output=/ocean/projects/cis230007p/palavall/nanoGPT/data/xsum_train/prepare.out
-#SBATCH --err=/ocean/projects/cis230007p/palavall/nanoGPT/data/xsum_train/prepare.err
+#SBATCH --output=/ocean/projects/cis230002p/palavall/nanoGPT/data/xsum_train/prepare.out
+#SBATCH --err=/ocean/projects/cis230002p/palavall/nanoGPT/data/xsum_train/prepare.err
 #SBATCH --time=8:00:00
 #SBATCH	--mem=16Gb
 #SBATCH --gpus=v100-16:1
@@ -9,6 +9,6 @@
 
 module load anaconda3
 conda activate mpalaval-research
-cd /ocean/projects/cis230007p/palavall/nanoGPT/data/xsum_train
+cd /ocean/projects/cis230002p/palavall/nanoGPT/data/xsum_train
 
 python3 prepare.py
